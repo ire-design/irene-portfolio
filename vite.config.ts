7ed025e7381c77c3e31c8c95cd1f3d2410cc58fb
Irene Musau <irenemwikalii04@gmail.com>
2025-08-16 12:48:51 +0300

    tailwind css corrections

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-/* import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
- */
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import tailwind from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwind(), // ✅ add tailwind plugin here
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
